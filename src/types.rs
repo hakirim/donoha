29579145e5431f0f5d37f5ceed21575fa72fd62d
s.i <stonhair@gmail.com>
2022-08-21 17:40:20 +0900

    Define type Servers to contain server's id and name
    close #2 .

---
+++ src/types.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Servers {
+    pub servers: Vec<Server>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Server {
+    pub id: String,
+    pub name: String
+}
